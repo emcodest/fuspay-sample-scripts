180540eee93ed1bc17a782afceffcd9cccfc4ef2
Emcode LLC <eeema9@gmail.com>
2023-10-05 16:58:29 +0100

    Update payout-ghana.php

--- php/payout-ghana.php
+++ php/payout-ghana.php
@@ -6,7 +6,7 @@ curl_setopt_array($curl, [
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, 
   CURLOPT_CUSTOMREQUEST => "POST",
   CURLOPT_POSTFIELDS => "{\n\t\"withdrawal_reference\": \"0001\",\n\t\"account_number\": \"xxxxxxxxx\",\n\t\"account_code\": \"MTN\",\n\t\"partner_id\": \"pxxxxx\",\n\t\"intrapay_merchant_id\": \"intra_merchant_xxxx\",\n\t\"amount\": 1,\n\t\"currency\": \"GHS\",\n\t\"signature\": \"6a7904a79006a21ba4dca3e751c990eadec383993a84f1d030934592582901c04e08b6cb51fe63f340865a3ebb82be08ff75bdea51eb228dbaaf85b56fa5613c\"\n}",
   CURLOPT_HTTPHEADER => [
